aebcf8aaa5152a377bde8eb72ffacb33444ab142
luannapereira <93163993+luannapereira@users.noreply.github.com>
2022-05-10 17:48:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+//scripts Luana Pereira
+
+var myButtonModal = document.getElementById('myButtonModal')
+var myTextModal = document.getElementById('myTextModal')
+
+var inputUsuario = document.getElementById('inputUsuario')
+var inputSenha = document.getElementById('inputSenha')
+
+function autenticar(event){
+  // escopo função autenticar
+  event.preventDefault()
+
+  if(inputUsuario.value == 'admin' && inputSenha.value == '1234'){
+    myTextModal.className = 'text-success'
+    myTextModal.innerText = 'Usuário Logado!!!'
+  } else {
+    myTextModal.className = 'text-danger'
+    myTextModal.innerText = 'Usuário ou senha inválidos!!!'
+  }
+
+  myButtonModal.click()
+}
